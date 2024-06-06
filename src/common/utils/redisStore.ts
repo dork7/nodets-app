13f99a0f2fc5440ab35ff23697618c54a30cc6b0
Hamza Mehmood <hamza.mahmood@zho.gov.ae>
2024-06-06 12:05:20 +0400

    fix: typescript fix added

--- src/common/utils/redisStore.ts
+++ src/common/utils/redisStore.ts
@@ -1,10 +1,11 @@
+import { IRedis } from '@/api/redis/redisModel';
 import { redisClient } from '@/config/redisStore';
 
 export const redis = {
  setValue: async (key: string, value: any): Promise<string | null> => {
   return redisClient.set(key, JSON.stringify(value));
  },
- getValue: async (key: string): Promise<{} | null> => {
+ getValue: async (key: string): Promise<IRedis | null> => {
   return redisClient.get(key);
  },
 };
