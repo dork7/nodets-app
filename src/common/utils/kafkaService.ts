1faf0c663efbe656c047879c55cd590a218e7d8d
Hamza Mehmood <hamza.mahmood@zho.gov.ae>
2024-06-24 15:31:07 +0400

    chore: kafka headers added

--- src/common/utils/kafkaService.ts
+++ src/common/utils/kafkaService.ts
@@ -1,15 +1,24 @@
 import { sendMessage } from '@/config/kafka';
 import { logger } from '@/server';
 
-export const readKafkaMessage = async ({ topic, partition, message }) => {
+export const readKafkaMessage = async ({ topic, partition, message, heartbeat, pause }) => {
  logger.info({
   topic,
   partition,
   offset: message.offset,
+  headers: parseHeaders(message.headers),
   value: JSON.parse(message.value.toString()),
  });
 };
 
 export const sendKafkaMessage = async (topic: string, message) => {
  return sendMessage(topic, JSON.stringify(message));
+};
+
+const parseHeaders = (headers: any) => {
+ return Object.keys(headers).map((item: any) => {
+  return {
+   [item]: Buffer.from(headers[item]).toString(),
+  };
+ });
 };
--- src/config/kafka.ts
+++ src/config/kafka.ts
@@ -2,11 +2,12 @@ import { Kafka } from 'kafkajs';
 
 import { TTopicList } from '@/api/kafka/kafkaModel';
 import { TOPIC_LIST } from '@/common/data/kafkaTopics';
+import { env } from '@/common/utils/envConfig';
 import { readKafkaMessage } from '@/common/utils/kafkaService';
 
 const kafka = new Kafka({
- clientId: 'my-app',
- brokers: ['localhost:29092'],
+ clientId: env.CLIENT_ID,
+ brokers: [env.KAFKA_BROKER],
 });
 
 const producer = kafka.producer();
@@ -15,7 +16,14 @@ const consumer = kafka.consumer({ groupId: 'test-group' });
 export const sendMessage = async (topic: string, message: any) => {
  return await producer.send({
   topic,
-  messages: [{ value: message }],
+  messages: [
+   {
+    key: 'key',
+    headers: { 'correlation-id': '2bfb68bb-893a-423b-a7fa-7b568cad5b67', 'system-id': 'my-system' },
+    value: message,
+   },
+  ],
+  acks: 1,
  });
 };
 
