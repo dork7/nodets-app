5dd46a32539a1620850ef2cf2ae05cca867e60cf
Hamza Mehmood <hamza.mahmood@zho.gov.ae>
2024-06-24 15:31:07 +0400

    chore: kafka headers added

--- src/common/utils/envConfig.ts
+++ src/common/utils/envConfig.ts
@@ -4,13 +4,15 @@ import { cleanEnv, host, num, port, str, testOnly } from 'envalid';
 dotenv.config();
 
 export const env = cleanEnv(process.env, {
-  NODE_ENV: str({ devDefault: testOnly('test'), choices: ['development', 'production', 'test'] }),
-  HOST: host({ devDefault: testOnly('localhost') }),
-  PORT: port({ devDefault: testOnly(3000) }),
-  CORS_ORIGIN: str({ devDefault: testOnly('http://localhost:2020') }),
-  COMMON_RATE_LIMIT_MAX_REQUESTS: num({ devDefault: testOnly(1000) }),
-  COMMON_RATE_LIMIT_WINDOW_MS: num({ devDefault: testOnly(1000) }),
-  REDIS_HOST: str(),
-  REDIS_PORT: num(),
-  ENV: str(),
+ NODE_ENV: str({ devDefault: testOnly('test'), choices: ['development', 'production', 'test'] }),
+ HOST: host({ devDefault: testOnly('localhost') }),
+ PORT: port({ devDefault: testOnly(3000) }),
+ CORS_ORIGIN: str({ devDefault: testOnly('http://localhost:2020') }),
+ COMMON_RATE_LIMIT_MAX_REQUESTS: num({ devDefault: testOnly(1000) }),
+ COMMON_RATE_LIMIT_WINDOW_MS: num({ devDefault: testOnly(1000) }),
+ REDIS_HOST: str(),
+ REDIS_PORT: num(),
+ KAFKA_BROKER: str(),
+ CLIENT_ID: str(),
+ ENV: str(),
 });
