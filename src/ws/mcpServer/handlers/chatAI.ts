fe5f16d8439ae7be405d4a23a0b9f97338d86016
hamza <hamza@hamzas-MacBook-Pro.local>
2025-10-23 09:00:17 +0400

    chore: chunk option added

--- src/ws/mcpServer/handlers/chatAI.ts
+++ src/ws/mcpServer/handlers/chatAI.ts
@@ -5,19 +5,28 @@ import { redis } from '@/services/redisStore';
 export const name = 'chatAI';
 export const handler = async (ws: any, message: any) => {
  const aiInput = message.params?.prompt || 'Hello, AI!';
+
+ const streamParam = message?.stream;
+ const stream = streamParam === 'false' || streamParam === false ? false : Boolean(streamParam);
+
  const hs = await historyObject(message.id);
  const newHistory = [...hs, { role: 'user', content: aiInput }];
  await saveHistory(message.id, newHistory);
 
- const aiResponse = await callAI(JSON.stringify(newHistory));
-
  ws.send(JSON.stringify({ sender: 'AI', type: 'stream_start', id: message.id }));
+ const aiResponse: any = await callAI(JSON.stringify(newHistory), stream);
 
- for await (const chunk of aiResponse) {
-  if (chunk.choices && chunk.choices[0]?.delta?.content) {
-   logger.info(`AI Response Chunk: , ${chunk.choices[0].delta.content}`);
-   ws.send(JSON.stringify({ sender: 'AI', type: 'stream_continue', aiResponse: chunk.choices[0].delta }));
+ if (stream) {
+  for await (const chunk of aiResponse) {
+   if (chunk.choices && chunk.choices[0]?.delta?.content) {
+    logger.info(`AI Response Chunk: , ${chunk.choices[0].delta.content}`);
+    ws.send(JSON.stringify({ sender: 'AI', type: 'stream_continue', aiResponse: chunk.choices[0].delta }));
+   }
   }
+ } else {
+  const fullResponse = await aiResponse.choices[0].message;
+  logger.info(`AI Full Response: , ${fullResponse}`);
+  ws.send(JSON.stringify({ sender: 'AI', type: 'stream_continue', aiResponse: fullResponse, id: message.id }));
  }
  ws.send(JSON.stringify({ sender: 'AI', type: 'stream_end', id: message.id }));
 };
